002c2264816b568c74f74bc3dbd8aa61ff46369d
ryankung0623 <arek.holdings@gmail.com>
2022-04-19 14:41:19 +1000

    modified env and added timer decorater

--- main.py
+++ main.py
@@ -4,28 +4,26 @@ import pandas as pd
 import os
 
 
-
-
-
-tickers = pd.read_csv(config['TICKERS_CSV_PATH'])
+tickers = pd.read_csv(f"tickers/{config['TICKERS_CSV_PATH']}")
 tickers = list(tickers.iloc[:,0])
-
-
 raw_data_dir = os.listdir(config['RAW_DATA_PATH'])
 
 crawler = Crawler()
+result = crawler.get('GOOG',config['EXCHANGE_CODE'])
 
-for ticker in tickers:
-        if ticker in raw_data_dir:
-                print(str(tickers.index(ticker)+1) + ". " + ticker + " done...")
-                continue
+# for ticker in tickers:
+#         if ticker in raw_data_dir:
+#                 print(str(tickers.index(ticker)+1) + ". " + ticker + " done...")
+#                 continue
 
-        msg = crawler.get(ticker)
-        if msg == 'success':
-            crawler.write(raw_data_dir + ticker)
-            print(str(tickers.index(ticker)+1) + ". " + ticker + " done...")
-        else:
-            print(msg)
-            continue
+#         msg = crawler.get(ticker)
+#         if msg == 'success':
+#             crawler.write(raw_data_dir + ticker)
+#             print(str(tickers.index(ticker)+1) + ". " + ticker + " done...")
+#         else:
+#             print(msg)
+#             continue
 
-crawler.quit()
+# crawler.quit()
+
+
