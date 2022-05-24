d1bafb0cf01a5cd50f77170efc76666aac3be264
ryankung0623 <arek.holdings@gmail.com>
2022-05-24 11:34:29 +1000

    changed from * to import as writer to avoid future conflict

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-from services.crawler import Crawler
 from helper.config import config
-import pandas as pd
-import os
+
+from services.crawler import Crawler
+import services.writer as writer
+
 import threading
 from tqdm import tqdm
+import pandas as pd
+
+import os
 import time
-from services.writer import *
 from itertools import cycle
 
 # turn off pandas warning
@@ -19,6 +22,7 @@ NUMBER_OF_THREADS = int(config['NUMBER_OF_THREADS'])
 
 tickers = pd.read_csv(config['TICKERS_CSV_PATH'], dtype = str)
 tickers = list(tickers.iloc[:,0])
+tickers = []
 
 thread_list = []
 workers = NUMBER_OF_THREADS
@@ -58,7 +62,7 @@ tickers.sort()
 # get all sector names and append tickers to the right sector
 sectors = dict()
 for ticker in tickers:
-    data = load_pickle(RAW_DATA_DIR + ticker)
+    data = writer.load_pickle(RAW_DATA_DIR + ticker)
     
     if data["Sector"] not in sectors.keys():
         sectors[data["Sector"]] = [ticker]
@@ -68,19 +72,19 @@ for ticker in tickers:
 for sector in sectors.keys():
     print(f'Compiling {sector} sector tearsheet...')
     #open a doc
-    document = Document()
+    document = writer.Document()
     # trim the margins down to 1cm on each side
     sections = document.sections
     for section in sections:
-        section.top_margin = Cm(1)
-        section.bottom_margin = Cm(1)
-        section.left_margin = Cm(1)
-        section.right_margin = Cm(1)
+        section.top_margin = writer.Cm(1)
+        section.bottom_margin = writer.Cm(1)
+        section.left_margin = writer.Cm(1)
+        section.right_margin = writer.Cm(1)
 
     #compose tear sheets sector by sector
     for ticker in tqdm(sectors[sector]):
         try:
-            data = load_pickle(RAW_DATA_DIR + ticker)
+            data = writer.load_pickle(RAW_DATA_DIR + ticker)
             document.add_heading(ticker, 0)
             p = document.add_paragraph()
 
@@ -91,25 +95,25 @@ for sector in sectors.keys():
                     continue
                 s = p.add_run("%s  -  %s\t\t"%(info,data[info]))
                 s.font.name = 'Arial'
-                s.font.size = Pt(8)
+                s.font.size = writer.t(8)
 
             # empty lines between quick summary and detail information
             s = p.add_run("\n\n")
 
             s = p.add_run(data['company description'])
             s.font.name = 'Arial'
-            s.font.size = Pt(8)
+            s.font.size = writer.Pt(8)
 
-            part1 = pd.concat([add_header(data['quick financials']),data['profitability ratio']])
-            df2table(document, part1)
+            part1 = pd.concat([writer.add_header(data['quick financials']),data['profitability ratio']])
+            writer.df2table(document, part1)
 
             p = document.add_paragraph()
 
-            #part2 = pd.concat([add_header(data['balance sheet']),data['liquidity']])
-            df2table(document, add_header(data['balance sheet']))
-            df2table(document, add_header(data['liquidity']))
+            #part2 = pd.concat([writer.add_header(data['balance sheet']),data['liquidity']])
+            writer.df2table(document, writer.add_header(data['balance sheet']))
+            writer.df2table(document, writer.add_header(data['liquidity']))
 
-            df2table(document, add_header(df_filter(data['efficiency'],['Days Sales Outstanding','Days Inventory','Payables Period','Cash Conversion Cycle'])))
+            writer.df2table(document, writer.add_header(writer.df_filter(data['efficiency'],['Days Sales Outstanding','Days Inventory','Payables Period','Cash Conversion Cycle'])))
 
         except Exception as e:
             print(e)
@@ -124,5 +128,6 @@ for sector in sectors.keys():
             pass
         document.save(f'{OUTPUT_PATH}{sector}.docx')
         
-# #convert all docs to pdfs
-# to_pdf(PDF_OUTPUT_PATH)
+#convert all docs to pdfs
+writer.to_pdf(OUTPUT_PATH)
+
--- services/crawler.py
+++ services/crawler.py
@@ -9,10 +9,7 @@ import pandas as pd
 import numpy as np
 import pickle
 import os
-
 import time
-
-
 
 class Crawler:
         def __init__(self):
